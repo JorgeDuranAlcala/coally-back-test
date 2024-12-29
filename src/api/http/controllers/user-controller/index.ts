6aa858d3a05f54f3d7a9860874fb105244e4fb72
Jorge Duran <jorgeluis21.duran@gmail.com>
2024-12-29 14:25:40 -0400

    modified

--- src/api/http/controllers/user-controller/index.ts
+++ src/api/http/controllers/user-controller/index.ts
@@ -1,6 +1,6 @@
 import { Validator } from "src/libs/Validator/validator";
 import { UserController } from "./user-controller";
-import { userService } from "src/application/services/User";
+import { userService } from "../../../../application/services/User";
 
 const validator = Validator.create();
 export const userController = new UserController(userService, validator);
